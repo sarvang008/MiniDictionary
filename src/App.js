3702ebbe144947f1ad3b417616f265af93f47d65
rahulpegasus <106219892+rahulpegasus@users.noreply.github.com>
2023-10-17 14:00:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Header from "./Components/Header";
 import Dashboard from "./Pages/Dictionary/Dashboard";
 function App() {
-  return <div className="cart"><Header/> <Dashboard/></div>;
+  
+  return <div className="cart"><div> Hiii </div><Header/> <Dashboard/></div>;
 }
 
 export default App;
